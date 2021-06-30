73877ae10eb2f43a11ff4338b1d2b19bf24ed4cc
Michael Cheng <yuenming54@gmail.com>
2021-07-01 02:22:57 +0800

    Add cool face API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const Dishes = require('../models/dishes');
 
-module.exports = router;
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors,(req,res,next) => {
+    Dishes.find({})
+    .populate('comments.author')
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+module.exports = dishRouter;
